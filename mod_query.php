0d08c6a067d44689604acb0bffb6907ea0df8b46
Derrick Egersdorfer <derrick@egersdorfer.co.za>
2015-10-12 22:58:52 +0200

    Initial commit

---
+++ mod_query.php
@@ -0,0 +1,87 @@
+<?php
+
+/**
+ * Web Query Module Entry Point
+ * 
+ * @package    Joomla
+ * @subpackage Modules
+ * @license    MIT
+ * @link
+ *     
+ */
+ 
+// No direct access
+defined('_JEXEC') or die;
+
+// Load vendors
+include 'vendor/autoload.php';
+
+// Load helpers
+include 'helpers/db.php';
+include 'helpers/mailer.php';
+
+// Gather FuelPHP
+use Fuel\Validation\Validator;
+
+// Settings
+$showSuburb = $params->get('suburb', false);
+$showProvince = $params->get('province', false);
+$showMessage = $params->get('message', false);
+$showBuySell = $params->get('buysell', false);
+
+// Check for post data
+if($post = JRequest::getVar('query', false, 'post')){
+
+    // Check honeypot
+    if( ! empty($_POST['birthday']) ){
+        return true;
+    }
+
+    // Validate
+    $val = new Validator;
+    $val->addField('name')->required();
+    $val->addField('number')->required()->number();
+    $showSuburb ? $val->addField('suburb')->required() : null;
+    $showProvince ? $val->addField('province')->required() : null;
+    $result = $val->run($post);
+    if($result->isValid()){
+
+        // Save data
+        QueryHelperDB::save($post);
+
+        // Email data
+        QueryHelperMailer::send(
+            array(
+                $params->get('to_a'), 
+                $params->get('to_b'), 
+                $params->get('to_c')
+            ),
+            array(
+                $params->get('cc_a'), 
+                $params->get('cc_b'), 
+                $params->get('cc_c')
+            ),
+            array(
+                $params->get('bcc_a'), 
+                $params->get('bcc_b'), 
+                $params->get('bcc_c')
+            ),
+            $params->get('subject'),
+            $post
+        );
+
+        // We done
+        print '<div class="ui message"><i class="ui circular checkmark icon"></i>Sent successfully, we will be in touch.</div>';
+
+        // Message
+        JFactory::getApplication()->enqueueMessage('Please check that all required fields have been completed.', 'success');
+    }
+
+}
+
+// Display data
+ob_start();
+    require JModuleHelper::getLayoutPath('mod_query', 'default');
+    $get = ob_get_contents();
+ob_end_clean();
+print preg_replace("({{ ?form ?}})", $get, $params->get('template'));
---
+++ tmpl/default.php
@@ -0,0 +1,163 @@
+<form class="ui query form" action="<?php print Juri::current();?>" method="post">
+    
+    <div class="field">
+        <input type="text" name="query[name]" placeholder="Full name">
+    </div>
+    
+    <div class="field">
+        <input type="text" name="query[number]" placeholder="Phone number">
+    </div>
+
+    <div class="field">
+        <input type="email" name="query[email]" placeholder="Email address">
+    </div>
+
+    <?php if($showSuburb) : ?>
+    <div class="field">
+        <input type="email" name="query[suburb]" placeholder="Enter a suburb name">
+    </div>
+    <?php endif; ?>
+
+    <?php if($showProvince) : ?>
+    <div class="field">
+        <div id="suburbs" class="ui fluid selection dropdown">
+            <input type="hidden" name="query[province]">
+            <div class="default text">Province</div>
+            <i class="dropdown icon"></i>
+            <div class="menu">
+                <div class="item" data-value="kwazulu-natal">Kwazulu Natal</div>
+                <div class="item" data-value="gauteng">Gauteng</div>
+                <div class="item" data-value="western-cape">Western Cape</div>
+                <div class="item" data-value="eastern-cape">Eastern Cape</div>
+                <div class="item" data-value="limpopo">Limpopo</div>
+                <div class="item" data-value="Other">Other</div>
+            </div>
+        </div>
+    </div>
+    <?php endif; ?>
+
+    <?php if($showMessage) : ?>
+    <div class="field">
+        <textarea name="query[message]" placeholder="Message"></textarea>
+    </div>
+    <?php endif; ?>
+
+    <?php if($showBuySell) : ?>
+    <div class="field">
+        <div id="suburbs" class="ui fluid selection dropdown">
+            <input type="hidden" name="query[buysell]">
+            <div class="default text">Interested in</div>
+            <i class="dropdown icon"></i>
+            <div class="menu">
+                <div class="item" data-value="buying">Buying</div>
+                <div class="item" data-value="selling">Selling</div>
+            </div>
+        </div>
+    </div>
+    <?php endif; ?>
+    
+    <div class="ui error message">
+        <div class="header">We noticed some issues</div>
+    </div>
+
+    <div class="ui right labeled query icon submit small button">Submit<i class="right arrow icon"></i></div>
+
+    <input type="hidden" name="query[birthday]" value="" style="display:none;" />
+
+    <input type="hidden" name="query[token]" value="<?php print uniqid(); ?>" />
+
+</form>
+
+<script>
+
+    // Ready state 
+    jQuery(document).ready(function() {
+        
+        // Dropdowns
+        $('#suburbs').dropdown();
+
+        // Numbers only for phone number
+        jQuery("input[name='query[number]']").keydown(function (e) {
+            var key = e.which || e.keyCode;
+
+            if (
+                !e.shiftKey && !e.altKey && !e.ctrlKey &&
+                // numbers
+                key >= 48 && key <= 57 ||
+                // Numeric keypad
+                key >= 96 && key <= 105 ||
+                // comma, period and minus, . on keypad
+                key == 190 || key == 188 || key == 109 || key == 110 ||
+                // Backspace and Tab and Enter
+                key == 8 || key == 9 || key == 13 ||
+                // Home and End
+                key == 35 || key == 36 ||
+                // left and right arrows
+                key == 37 || key == 39 ||
+                // Del and Ins
+                key == 46 || key == 45
+            ){
+                return true;
+            }else{
+                return false;
+            }
+        });
+
+        // Validation
+        $('.ui.query.form').form({
+            fields: {
+                fullName: {
+                identifier  : 'query[name]',
+                rules: [{
+                        type   : 'empty',
+                        prompt : 'Please enter your full name'
+                    }]
+                },
+                number: {
+                identifier  : 'query[number]',
+                rules: [{
+                        type   : 'empty',
+                        prompt : 'Please enter your contact number'
+                    },{
+                        type : 'length[10]',
+                        prompt : 'Your contact number is too short'
+                    },{
+                        type : 'maxLength[10]',
+                        prompt : 'Your contact number is too long'
+                    }]
+                },
+                email: {
+                identifier  : 'query[email]',
+                rules: [{
+                        type   : 'empty',
+                        prompt : 'Please enter your email address'
+                    },{
+                        type   : 'email',
+                        prompt : 'Please enter a valid email address'
+                    }]
+                },
+                province: {
+                identifier  : 'query[province]',
+                rules: [{
+                        type   : 'empty',
+                        prompt : 'Please enter your province'
+                    }]
+                },
+                suburb: {
+                identifier  : 'query[suburb]',
+                rules: [{
+                        type   : 'empty',
+                        prompt : 'Please enter your your suburb'
+                    }]
+                }
+            }
+        });
+
+        // Submit the form on click
+        jQuery(".ui.query.button").click(function(){
+            jQuery(".ui.query.form").submit();
+        });
+
+    });
+
+</script>
